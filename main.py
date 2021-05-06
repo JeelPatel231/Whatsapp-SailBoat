1104391bb4af90fbed7ee03a0ca9f3c5a16d4d35
jeelpatel231 <jeelpatel231@gmail.com>
2021-05-06 19:55:47 +0530

    add is replied functionality and active chat function

--- main.py
+++ main.py
@@ -27,20 +27,30 @@ def get_nth_chat(n):
 def get_latest_msg():
     try:
         latest_msg = driver.find_element_by_xpath("//div[@aria-label='Chat list']/div[contains(@style,'transform: translateY(0px)')]/div/div/div[2]/div[2]/div[1]/span/span").text
-        driver.find_element_by_xpath("//div[@aria-label='Chat list']/div[contains(@style,'transform: translateY(0px)')]/div/div/div[2]/div[2]/div[1]/span/div/span")
-        latest_msg_from = "Me"
+        try:
+            driver.find_element_by_xpath("//div[@aria-label='Chat list']/div[contains(@style,'transform: translateY(0px)')]/div/div/div[2]/div[2]/div[1]/span/div/span[@data-testid='status-check']")
+            latest_msg_from = "Me"
+        except:
+            driver.find_element_by_xpath("//div[@aria-label='Chat list']/div[contains(@style,'transform: translateY(0px)')]/div/div/div[2]/div[2]/div[1]/span/div/span[@data-testid='status-dblcheck']")
+            latest_msg_from = "Me"
+        chat_type = "noidea"
     except:
-        latest_msg = driver.find_element_by_xpath("//div[@aria-label='Chat list']/div[contains(@style,'transform: translateY(0px)')]/div/div/div[2]/div[2]/div[1]/span/span[3]").text
-        latest_msg_from = driver.find_element_by_xpath("//div[@aria-label='Chat list']/div[contains(@style,'transform: translateY(0px)')]/div/div/div[2]/div[2]/div[1]/span/span[1]").text
+        try:
+            latest_msg = driver.find_element_by_xpath("//div[@aria-label='Chat list']/div[contains(@style,'transform: translateY(0px)')]/div/div/div[2]/div[2]/div[1]/span/span[3]").text
+            latest_msg_from = driver.find_element_by_xpath("//div[@aria-label='Chat list']/div[contains(@style,'transform: translateY(0px)')]/div/div/div[2]/div[2]/div[1]/span/span[1]").text
+            chat_type = "group"
+        except:
+            latest_msg = driver.find_element_by_xpath("//div[@aria-label='Chat list']/div[contains(@style,'transform: translateY(0px)')]/div/div/div[2]/div[2]/div[1]/span/span").text
+            chat_type = "personal"
+            latest_msg_from = driver.find_element_by_xpath("//div[@aria-label='Chat list']/div[contains(@style,'transform: translateY(0px)')]/div/div/div[2]/div[1]/div[1]/span").text
     print(latest_msg)
+    print(chat_type)
     print(latest_msg_from)
 
 def select_chat(chat_name):
-    # search_input_box = 
     driver.find_element_by_xpath("//div[@id='side']/div[1]/div/label/div/div[2]").send_keys(chat_name + Keys.ENTER)
-    # get_nth_chat(1).click()
 
-def write_msg(text):
+def send_msg(text):
     driver.find_element_by_xpath("//footer/div[1]/div[2]/div/div[2]").send_keys(text)
     driver.find_element_by_xpath("//footer/div[1]/div[3]/button/span").click()
 
@@ -50,8 +60,16 @@ def scanqr():
     driver.get_screenshot_as_file("screenshot.png")
 scanqr()
 
-
-
-
-##################### functions start here ########################
-
+def active_chat_last():
+    last_msg = driver.find_element_by_xpath("//div[@role='region']/div[last()]/div/div/div/div[last()-1]/div[last()]").text
+    try:
+        driver.find_element_by_xpath("//div[@role='region']/div[last()]/div/div/div/div[last()-1]/div[last()-1]").text
+        is_replied = True
+    except:
+        is_replied = False
+    last_msg_time = driver.find_element_by_xpath("//div[@role='region']/div[last()]/div/div/div/div[last()]").text
+    try:
+        last_msg_sender = driver.find_element_by_xpath("//div[@role='region']/div[last()]/div/div/div/div[last()-2]").text
+    except:
+        last_msg_sender = "me"
+    print(last_msg,"\n",last_msg_sender,"\n",last_msg_time,"\n",is_replied)
